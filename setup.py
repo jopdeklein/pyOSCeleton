30475049702834a476afe19f99925627126cde47
Jop de Klein <jop@validweb.nl>
2011-11-25 22:32:18 +0100

    OSCeleton hand mode compatibility.
    Quick & Dirty hacking to get pyOSCeleton to work with OSCeleton's hand
    mode, prevent IndexErrors when hand is lost. Added setup.py for
    pip support.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyOSCeleton',
+      version='1.0',
+      description='pyOSCeleton',
+      author='Robbie Clemons',
+      url='http://gitorious.org/pyosceleton',
+      py_modules=['OSCeleton'],
+     )
